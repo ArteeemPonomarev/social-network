add42e37be59d8f2d4e7c55e2c3bb10de16f4c96
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-04-10 11:15:15 +0400

    Updates

---
+++ precache-manifest.d94d954db17074b9a06b682967356925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52625309c04558c74e6a5338799b1dd",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "292dd2213e2d57a3675d",
+    "url": "/social-network/static/css/main.9516e029.chunk.css"
+  },
+  {
+    "revision": "cba22bba0b0862d166cc",
+    "url": "/social-network/static/js/2.539c1f13.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/social-network/static/js/2.539c1f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292dd2213e2d57a3675d",
+    "url": "/social-network/static/js/main.65f926d2.chunk.js"
+  },
+  {
+    "revision": "b4e47c17a0c124211064",
+    "url": "/social-network/static/js/runtime-main.0a7301e8.js"
+  }
+]);
