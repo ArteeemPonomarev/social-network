c2c9edb6615a418a17b7271c36ff4a768e3b22a8
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-04-10 13:36:14 +0400

    Updates

--- precache-manifest.31163bad823b8891de3484dae3f8d9b2.js
+++ precache-manifest.31163bad823b8891de3484dae3f8d9b2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "888f02177fbfd7e9519d928f7d048d22",
+    "revision": "3b06c94457f08852c5d62f257fd9b63b",
     "url": "/social-network/index.html"
   },
   {
-    "revision": "78ab07643e652829d09f",
+    "revision": "81edaf757fcfe79ecd82",
     "url": "/social-network/static/css/main.9516e029.chunk.css"
   },
   {
-    "revision": "cba22bba0b0862d166cc",
-    "url": "/social-network/static/js/2.539c1f13.chunk.js"
+    "revision": "e3cad76c86b3da97e9ee",
+    "url": "/social-network/static/js/2.7735707d.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/social-network/static/js/2.539c1f13.chunk.js.LICENSE.txt"
+    "url": "/social-network/static/js/2.7735707d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "78ab07643e652829d09f",
-    "url": "/social-network/static/js/main.05c8cef9.chunk.js"
+    "revision": "81edaf757fcfe79ecd82",
+    "url": "/social-network/static/js/main.c8e0c0e8.chunk.js"
   },
   {
     "revision": "b4e47c17a0c124211064",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-network/precache-manifest.73f1f2e7db2b80c588fcc3a49f4dfd7c.js"
+  "/social-network/precache-manifest.31163bad823b8891de3484dae3f8d9b2.js"
 );
 
 self.addEventListener('message', (event) => {
