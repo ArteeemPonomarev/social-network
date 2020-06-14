148c1edd5e1665f19eb0e6e2791a90ab924c7475
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-06-14 16:47:52 +0300

    61, auth/me, login

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,6 +3,7 @@ import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import sidebarReducer from './sidebar-reducer';
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 
 let reducers = combineReducers({
@@ -10,6 +11,7 @@ let reducers = combineReducers({
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     userPage: usersReducer,
+    auth: authReducer,
 });
 
 
