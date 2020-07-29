47e2318545bbe168b29fbf96e04ea5aaaf0e52f0
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-07-29 15:01:24 +0300

    87, shouldComponentUpdate, PureComponent, React.memo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,10 +6,28 @@ import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormsControls/FormsControls";
 
 const maxLength10 = maxLengthCreator(10)
-const MyPosts = (props) => {
 
-    let postsElement = props.profilePage.postsData.map(p => <Post message={p.post} likesCounter={p.likes}/>);
+const AddNewPost = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component={Textarea} validate={[required, maxLength10]} name={'addNewPost'}
+                       placeholder={'Enter your message'}/>
+            </div>
+            <div className={s.buttons}>
+                <button>Add post</button>
+                <button>Remove</button>
+            </div>
+        </form>
+    )
+}
 
+const AddNewPostRedux = reduxForm({form: 'addNewPost'})(AddNewPost)
+
+const MyPosts = React.memo(props => {
+    console.log('render')
+    let postsElement = props.profilePage.postsData.map(p => <Post message={p.post}
+                                                                  likesCounter={p.likes}/>);
 
     let sendNewPost = (values) => {
         props.addPost(values.addNewPost)
@@ -30,22 +48,7 @@ const MyPosts = (props) => {
             </div>
         </div>
     );
-}
+})
 
-const AddNewPost = (props) => {
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <div>
-                <Field component={Textarea} validate={[required,maxLength10]} name={'addNewPost'} placeholder={'Enter your message'}/>
-            </div>
-            <div className={s.buttons}>
-                <button>Add post</button>
-                <button>Remove</button>
-            </div>
-        </form>
-    )
-}
-
-const AddNewPostRedux = reduxForm({form: 'addNewPost'})(AddNewPost)
 
 export default MyPosts;
