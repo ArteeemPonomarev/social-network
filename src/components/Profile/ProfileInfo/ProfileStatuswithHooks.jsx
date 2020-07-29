d752ba3924e3eb4f842b8a1393096818eac7fb79
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-07-29 14:02:22 +0300

    85, useEffect

--- src/components/Profile/ProfileInfo/ProfileStatuswithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatuswithHooks.jsx
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 const ProfileStatuswithHooks = (props) => {
+
+    useEffect(() => {
+         setStatus(props.status )
+    }, [props.status])
 
     const [isEditMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
