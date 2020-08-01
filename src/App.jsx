c4af52e0daf96aa69265c12626654dc28767fa17
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-08-01 06:54:21 +0300

    95, gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route, withRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, withRouter} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
@@ -67,11 +67,11 @@ const AppContainer = compose(withRouter, connect(mapStateToProps, {initializedAp
 
 const SocialNetApp = (props) => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <AppContainer/>,
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
